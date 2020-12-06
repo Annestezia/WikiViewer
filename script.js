0e0acf243008470498be9989c3e758a3d779a03d
Annestezia <cast.wheel@gmail.com>
2020-12-06 22:28:35 +1000

    added favicon, spacing to results

--- script.js
+++ script.js
@@ -1,39 +1,50 @@
+
+
+
 $(document).ready(function(){
  // all about input
   $('#reset').on('click', function(){
     $('#searchInput').val('');
     $('#result').text('');
-  })
+  });
   $('#searchInput').keyup(function(e){
     if(e.which == 13){
       $('#searchButton').click();
     }
   });  
-  $('#searchButton').click(function(){
-    let input = $('#searchInput').val();
-    // here goes api call  and putting  output together
 
+  $('#searchButton').click(function(){
+    const input = $('#searchInput').val();
+    // here goes api call  and putting  output together
     const wikiAPIEndpoint = 'https://en.wikipedia.org/w/api.php';
-    let queryLink = wikiAPIEndpoint + '?action=query&list=search&format=json&continue=-%7C%7C&utf8=1&srsearch=' + encodeURI(input) + '&srlimit=10&origin=*';
-     // $('.resultHeader').hide().fadeIn('fast');
-     $('#result').empty();
+
+    const queryLink = `${wikiAPIEndpoint}?action=query&list=search&format=json&continue=-%7C%7C&utf8=1&srsearch=${encodeURI(input)}&srlimit=10&prop=url&origin=*&continue`;
+    $('#result').empty();
     
     //get JSON
     $.getJSON(queryLink, function(obj){
-     let title,
-         snippet,
-         articleURL;
-      if (obj.query.search.length!==0) {
-         for( let i = 0; i<obj.query.search.length; i++){
-         title = obj.query.search[i].title;
-         snippet = obj.query.search[i].snippet;
-         articleURL = 'https://en.wikipedia.org/wiki/' + title;
-         let articleDiv ='<a href="' + articleURL + '"><div class="article"><h4>' +title+'</h4>'+ snippet + '...</div></a>';
-         $('#result').append(articleDiv).hide().fadeIn('fast');
-         // $(articleDiv).hide().appendTo("#result").fadeIn('fast');
-            } 
-      } else { let  input = $('#searchInput').val();
-        $('#result').html('<h2>Oops... Can\'t find anything  like ' + input + ' :( <br />Try something else</h2>');
+      const searchResult = obj.query.search;
+      console.log(obj);
+
+      if (searchResult.length!==0) {
+        $.each(searchResult,(i, value)=>{
+          const {title, snippet, pageid}=value;
+          const articleURL = `https://en.wikipedia.org/wiki/${title}`;
+          const articleDiv =`<a href="${articleURL}"><div class="article"><h4> ${title}</h4>${snippet}...</div></a>`;
+          const more = `<a href="" class="btn btn-success" id="moreBtn">Load more >></a>`;
+
+          $('#result').append(articleDiv).hide().fadeIn(500);
+          if(i===searchResult.length-1&&!obj.batchcomplete){            
+            $('#result').append(more);
+            $("#moreBtn").click(e=>{
+              e.preventDefault(); 
+            });            
+          }
+        });
+
+
+      } else { const  input = $('#searchInput').val();
+        $('#result').html(`<h2>Oops... Can\'t find anything  like ${input} :( <br />Try something else</h2>`);
       }
    }); 
   });
